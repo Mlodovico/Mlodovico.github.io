576b252549228617d07754a3dfeced4aff545c14
Murilo Lodovico <37813008+Mlodovico@users.noreply.github.com>
2023-06-27 01:50:04 -0300

    feat: separete sectors and adding some styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Player } from '@lottiefiles/react-lottie-player';
+import develop1 from './assets/develop_1.json'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="category-sector">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello World
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="body-sector">
+        <div>
+
+        <h3>
+        Olá, seja bem vindo ao meu portifólio!!
+        </h3>
+
+        <p>
+          Meu nome é Murilo Lodovico, trabalho com desenvolvimento de software a aproximadamente 4 (quatro) anos. Formado em Ciência da Computaçāo pela Universade Paulista (UNIP) em 2022
+        </p>
+
+        <Player 
+          src={develop1}
+          className="player"
+          loop
+          autoplay
+          />
+          </div>
+      </div>
     </div>
   );
 }
