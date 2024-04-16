9caab9d353d2be5f68ec2f57d55095dbd6c7cca8
mlodovico <murilo.lodovico@outlook.com>
2024-04-16 12:31:31 -0300

    feature: format

--- src/ling/en-us.js
+++ src/ling/en-us.js
@@ -1,28 +1,38 @@
 export const enUs = {
-    firtsSubtitle:"Software Engineer",
-    yearsOld: '26 years',
-    socialMedia: 'Social Media',
-    resume: 'Resume',
-    knowledges: 'knowledges',
-    secondDivFirstSubtitle: 'Hello, welcome to a brief introduction to my professional background.',
-    secondDivFirstParagraph:"I began my journey in the programming field at the age of 16, exploring Arduino, C, and HTML. Subsequently, in 2018, I enrolled in the Computer Science program at university, completing it in 2021. During this period, I had the opportunity to work with and study the most widely used programming languages and frameworks, including Java, Python, React, Angular, Node, Swift, among others. This experience allowed me to gain knowledge in various areas, understanding the cycles and stages of projects.",
-    secondDivSecondParagraph: "I specialized in frameworks developed in JavaScript, such as Angular, React, React Native, Node, among others. However, this did not prevent me from expanding my knowledge in other areas, especially in backend development. I have always recognized that continuous learning and staying updated are crucial in the field I chose, and I have a passion for exploring new technologies, testing them, and continuing to learn.",
-    firstScrollviewCardCardTitle: "Projects",
-    secondScrollviewCardTitle: "Degrees and certifications",
-    goBarberDescription: 'Barber shop project, using React, React Native and Node',
-    gitHunterDescription: 'A system capable of searching for accounts in github by id, using React',
-    mostUsedWorldsDescription: "Vuejs project, capable of calculating the number of words in a given text",
-    habitsDescription: 'event marker and habits made in React',
-    degreeDescription: 'Cience Computer Degree Certificate',
-    swiftDegreeDescription: 'Certificate of Swift Degree',
-    javaSpringDegreeDescription: 'Certificate of Java and Spring',
-    webCourseDegreeDescription: 'Udemy Course, developing and learning JavaScript and its frameworks',
-    intelliJCourseDescription: 'Udemy Course for optimizing usability of IntelliJ',
-    checkInsDescription: 'Check Ins its a Java Springboot API project maded using lombok and other dependencies',
-    petshopDescription: 'Petshop Rest API, developed with NestJS and TypeORM, with MySQL database, and docker containers',
-    lang: 'Choose language',
-    lang1: 'Portuguese',
-    lang2: 'English',
-    lang3: 'Spanish',
-    portfolio: 'Portfolio',
-}
+  firtsSubtitle: "Software Engineer",
+  yearsOld: "26 years",
+  socialMedia: "Social Media",
+  resume: "Resume",
+  knowledges: "knowledges",
+  secondDivFirstSubtitle:
+    "Hello, welcome to a brief introduction to my professional background.",
+  secondDivFirstParagraph:
+    "I began my journey in the programming field at the age of 16, exploring Arduino, C, and HTML. Subsequently, in 2018, I enrolled in the Computer Science program at university, completing it in 2021. During this period, I had the opportunity to work with and study the most widely used programming languages and frameworks, including Java, Python, React, Angular, Node, Swift, among others. This experience allowed me to gain knowledge in various areas, understanding the cycles and stages of projects.",
+  secondDivSecondParagraph:
+    "I specialized in frameworks developed in JavaScript, such as Angular, React, React Native, Node, among others. However, this did not prevent me from expanding my knowledge in other areas, especially in backend development. I have always recognized that continuous learning and staying updated are crucial in the field I chose, and I have a passion for exploring new technologies, testing them, and continuing to learn.",
+  firstScrollviewCardCardTitle: "Projects",
+  secondScrollviewCardTitle: "Degrees and certifications",
+  goBarberDescription:
+    "Barber shop project, using React, React Native and Node",
+  gitHunterDescription:
+    "A system capable of searching for accounts in github by id, using React",
+  mostUsedWorldsDescription:
+    "Vuejs project, capable of calculating the number of words in a given text",
+  habitsDescription: "event marker and habits made in React",
+  degreeDescription: "Cience Computer Degree Certificate",
+  swiftDegreeDescription: "Certificate of Swift Degree",
+  javaSpringDegreeDescription: "Certificate of Java and Spring",
+  webCourseDegreeDescription:
+    "Udemy Course, developing and learning JavaScript and its frameworks",
+  intelliJCourseDescription:
+    "Udemy Course for optimizing usability of IntelliJ",
+  checkInsDescription:
+    "Check Ins its a Java Springboot API project maded using lombok and other dependencies",
+  petshopDescription:
+    "Petshop Rest API, developed with NestJS and TypeORM, with MySQL database, and docker containers",
+  lang: "Choose language",
+  lang1: "Portuguese",
+  lang2: "English",
+  lang3: "Spanish",
+  portfolio: "Portfolio",
+};
--- src/ling/pt-br.js
+++ src/ling/pt-br.js
@@ -1,28 +1,39 @@
 export const ptBR = {
-    firtsSubtitle:"Desenvolvedor Full-Stack",
-    yearsOld: '26 anos',
-    socialMedia: 'Redes Sociais',
-    resume: 'Curriculo',
-    knowledges: 'Conhecimentos',
-    secondDivFirstSubtitle: 'Olá, seja bem-vindo a uma breve introducão ao meu histórico profissional.',
-    secondDivFirstParagraph:"Iniciei minha jornada na área de programação aos 16 anos, explorando Arduino, C e HTML. Posteriormente, em 2018, ingressei na faculdade de Ciência da Computação, concluindo-a em 2021. Durante esse período, tive a oportunidade de trabalhar e estudar as linguagens de programação mais utilizadas, bem como os frameworks, incluindo Java, Python, React, Angular, Node, Swift, entre outros. Essa experiência me permitiu adquirir conhecimento em diversas áreas, compreendendo os ciclos e estágios dos projetos.",
-    secondDivSecondParagraph: "Especializei-me em frameworks desenvolvidos em JavaScript, como Angular, React, React Native, Node, entre outros. No entanto, isso não me impediu de expandir meus conhecimentos em outras áreas, especialmente em backend. Sempre reconheci que o constante aprendizado e atualização são fundamentais na área que escolhi, e tenho uma paixão por explorar novas tecnologias, testá-las e continuar aprendendo.",
-    firstScrollviewCardCardTitle: "Projetos",
-    secondScrollviewCardTitle: "Formaçōes e certificados",
-    goBarberDescription: 'Projeto de um sistema de barbearia, utilizando React, React Native e Node',
-    gitHunterDescription: 'Um sistema capaz de procurar contas no github pelo id, utilizando React',
-    mostUsedWorldsDescription: "Projeto feito com VueJs, capaz de calcular a quantidade de palavras de um devido texto",
-    habitsDescription: 'Marcador de complomissos e habitos feito em React',
-    degreeDescription: 'Certificado de formação acadêmica em Ciência da Computação',
-    swiftDegreeDescription: 'Certificado de formação Swift',
-    javaSpringDegreeDescription: 'Certificado de formação Java e Spring',
-    webCourseDegreeDescription: 'Curso Udemy, desenvolvendo e aprendendo a utilizar JavaScript e seus frameworks',
-    intelliJCourseDescription: 'Curso Udemy para otimizacao de usabilidade do IntelliJ',
-    checkInsDescription: 'Check Ins é um projeto de API Java Springboot feito usando Lombok e outras dependências',
-    petshopDescription: 'API Rest de Petshop, desenvolvida com NestJS e TypeORM, com banco de dados MySQL',
-    lang: 'Escolha idioma',
-    lang1: 'Portugues',
-    lang2: 'Ingles',
-    lang3: 'Espanhol',
-    portfolio: 'Portfólio'
-}
+  firtsSubtitle: "Desenvolvedor Full-Stack",
+  yearsOld: "26 anos",
+  socialMedia: "Redes Sociais",
+  resume: "Curriculo",
+  knowledges: "Conhecimentos",
+  secondDivFirstSubtitle:
+    "Olá, seja bem-vindo a uma breve introducão ao meu histórico profissional.",
+  secondDivFirstParagraph:
+    "Iniciei minha jornada na área de programação aos 16 anos, explorando Arduino, C e HTML. Posteriormente, em 2018, ingressei na faculdade de Ciência da Computação, concluindo-a em 2021. Durante esse período, tive a oportunidade de trabalhar e estudar as linguagens de programação mais utilizadas, bem como os frameworks, incluindo Java, Python, React, Angular, Node, Swift, entre outros. Essa experiência me permitiu adquirir conhecimento em diversas áreas, compreendendo os ciclos e estágios dos projetos.",
+  secondDivSecondParagraph:
+    "Especializei-me em frameworks desenvolvidos em JavaScript, como Angular, React, React Native, Node, entre outros. No entanto, isso não me impediu de expandir meus conhecimentos em outras áreas, especialmente em backend. Sempre reconheci que o constante aprendizado e atualização são fundamentais na área que escolhi, e tenho uma paixão por explorar novas tecnologias, testá-las e continuar aprendendo.",
+  firstScrollviewCardCardTitle: "Projetos",
+  secondScrollviewCardTitle: "Formaçōes e certificados",
+  goBarberDescription:
+    "Projeto de um sistema de barbearia, utilizando React, React Native e Node",
+  gitHunterDescription:
+    "Um sistema capaz de procurar contas no github pelo id, utilizando React",
+  mostUsedWorldsDescription:
+    "Projeto feito com VueJs, capaz de calcular a quantidade de palavras de um devido texto",
+  habitsDescription: "Marcador de complomissos e habitos feito em React",
+  degreeDescription:
+    "Certificado de formação acadêmica em Ciência da Computação",
+  swiftDegreeDescription: "Certificado de formação Swift",
+  javaSpringDegreeDescription: "Certificado de formação Java e Spring",
+  webCourseDegreeDescription:
+    "Curso Udemy, desenvolvendo e aprendendo a utilizar JavaScript e seus frameworks",
+  intelliJCourseDescription:
+    "Curso Udemy para otimizacao de usabilidade do IntelliJ",
+  checkInsDescription:
+    "Check Ins é um projeto de API Java Springboot feito usando Lombok e outras dependências",
+  petshopDescription:
+    "API Rest de Petshop, desenvolvida com NestJS e TypeORM, com banco de dados MySQL",
+  lang: "Escolha idioma",
+  lang1: "Portugues",
+  lang2: "Ingles",
+  lang3: "Espanhol",
+  portfolio: "Portfólio",
+};
